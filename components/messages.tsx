b165899e294bb6f79665a6b87434142f08cc064c
PranavKumar9529desai <dpranav7745@gmail.com>
2025-01-23 14:47:28 +0530

    feat: add enhanced markdown component and update dependencies for improved functionality

--- components/messages.tsx
+++ components/messages.tsx
@@ -1,10 +1,10 @@
-import { ChatRequestOptions, Message } from 'ai';
-import { PreviewMessage, ThinkingMessage } from './message';
-import { useScrollToBottom } from './use-scroll-to-bottom';
-import { Overview } from './overview';
-import { memo } from 'react';
-import { Vote } from '@/lib/db/schema';
-import equal from 'fast-deep-equal';
+import { ChatRequestOptions, Message } from "ai";
+import { PreviewMessage, ThinkingMessage } from "./message";
+import { useScrollToBottom } from "./use-scroll-to-bottom";
+import { Overview } from "./overview";
+import { memo } from "react";
+import { Vote } from "@/lib/db/schema";
+import equal from "fast-deep-equal";
 
 interface MessagesProps {
   chatId: string;
@@ -12,10 +12,10 @@ interface MessagesProps {
   votes: Array<Vote> | undefined;
   messages: Array<Message>;
   setMessages: (
-    messages: Message[] | ((messages: Message[]) => Message[]),
+    messages: Message[] | ((messages: Message[]) => Message[])
   ) => void;
   reload: (
-    chatRequestOptions?: ChatRequestOptions,
+    chatRequestOptions?: ChatRequestOptions
   ) => Promise<string | null | undefined>;
   isReadonly: boolean;
   isBlockVisible: boolean;
@@ -38,8 +38,7 @@ function PureMessages({
       ref={messagesContainerRef}
       className="flex flex-col min-w-0 gap-6 flex-1 overflow-y-scroll pt-4"
     >
-      {<Overview />} {/* Always show Overview at the start */}
-
+      {<Overview />}{" "}
       {messages.map((message, index) => (
         <PreviewMessage
           key={message.id}
@@ -56,11 +55,9 @@ function PureMessages({
           isReadonly={isReadonly}
         />
       ))}
-
       {isLoading &&
         messages.length > 0 &&
-        messages[messages.length - 1].role === 'user' && <ThinkingMessage />}
-
+        messages[messages.length - 1].role === "user" && <ThinkingMessage />}
       <div
         ref={messagesEndRef}
         className="shrink-0 min-w-[24px] min-h-[24px]"
